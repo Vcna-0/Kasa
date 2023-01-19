01dda23b59aa6baf830bf3fad64a3174f2dc8146
Noëmie <ncav@protonmail.com>
2023-01-19 15:00:49 +0100

    add error page

---
+++ src/pages/Error/Error.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './Error.module.css'
+
+export default function Error() {
+  return (
+    <div className= { styles.content}>
+      <div className= {styles.contentTitles}>
+        <h1 className= { styles.codeError }>404</h1>
+        <h2 className= { styles.message }>Oups! La page que vous demandez n'existe pas.</h2>
+      </div>
+      <Link className= { styles.link } to ="/">Retourner sur la page d'accueil</Link>
+    </div>
+  )
+}
