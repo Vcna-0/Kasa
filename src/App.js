fd21cc177c8d53e0eb737323d3ed940b3d0f2a1d
Noëmie <ncav@protonmail.com>
2023-01-19 14:57:32 +0100

    add basic files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, BrowserRouter } from "react-router-dom"
+import Home from "./pages/Home/Home"
+import About from "./pages/About/About"
+import HousingDetail from "./pages/HousingDetails/HousingDetails"
+import Error from "./pages/Error/Error"
+import styles from "./App.module.css"
+import Header from './components/Header/Header';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={ <Home/> } />
+        <Route path="/housingDetail/:id" element={ <HousingDetail/> } />
+        <Route path="/about" element={ <About/> } />
+        <Route path="*" element={ <Error/> } />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
